7c4743ead94d0cdf57fec66ff69e0029c69edc2a
David Gann <dgann@mainstreamdata.com>
2025-03-15 22:06:59 -0600

    WIP. Building auth APIs

--- backend/server/controllers/authController.js
+++ backend/server/controllers/authController.js
@@ -2,6 +2,7 @@ import {User} from "../../database/model.js";
 import { Op } from "sequelize";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
+const saltRounds = 10;  // How much salt to use for bcrypt
 
 
 
@@ -32,25 +33,61 @@ const authController = {
             });
         }
         // Ready to generate a new user
-        const salt = await bcrypt.genSalt(10);
-        const hashedPassword = bcrypt.hash(password, salt);
+        const salt = await bcrypt.genSalt(saltRounds);
+        const hashedPassword = await bcrypt.hash(password, salt);
         const user = await User.create({
             username,
             email,
             password: hashedPassword,
         })
+        // Give them an access token
+        const accessToken = jwt.sign({ userId: user.userId }, process.env.JWT_ACCESS_SECRET, {expiresIn: "15m"})
+        // And a refresh token
+        const refreshToken = jwt.sign({ userId: user.userId }, process.env.JWT_REFRESH_SECRET, {expiresIn: "7d"})
+        // Create access token (stores securely in client cookie) 
+        res.cookie("accessToken", accessToken, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === "production",
+            sameSite: "Strict",
+            maxAge: 15 * 60 * 1000, // 15 minutes
+        });
+        // Create refresh token for refreshing the session (stores securely in client cookie) 
+        res.cookie("refreshToken", refreshToken, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === "production",
+            sameSite: "Strict",
+            maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+        });
+        return res.status(201).json({
+            success: true,
+            message: "Registration successful",
+            user: {username, email, userId: user.userId},
+        });
 
-
-
-        res.status(201).json({ message: 'User registered successfully' });
       } catch (error) {
-        res.status(500).json({ error: error.message });
+        console.error(error);
+        return res.status(500).json({
+            success: false,
+            message: "Server error during registration",
+        });
       }
     },
-    login: async (req, res) => {
+
+    loginUsername: async (req, res) => {
       try {
         const { username, password } = req.body;
-        // Login logic
+        
+        const user = await User.findOne({
+            where: {username}
+        })
+        if (!user) {
+            return res.status(400).json({
+                success: false,
+                message: "Invalid credentials",
+            })
+        }
+        const isMatch = await bcrypt.compare(user.password)
+
         res.status(200).json({ message: 'Login successful' });
       } catch (error) {
         res.status(500).json({ error: error.message });
